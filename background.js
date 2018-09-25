1a3124cf8fd6309bdc534b435d7505ed9c8d5904
Robert Livaudais <robert.h.livaudais.19@dartmouth.edu>
2018-09-25 18:43:39 -0400

    logging landmark description now

--- background.js
+++ background.js
@@ -1,10 +1,40 @@
+const API_URL = 'https://vision.googleapis.com/v1/images:annotate?key=';
+
 // A generic onclick callback function.
 function genericOnClick(info, tab) {
-  console.log("info: " + JSON.stringify(info));
-  console.log(info);
-  let srcURL = info.srcUrl;
-  console.log(srcURL);
-  chrome.tabs.create({ url: srcURL });
+  let srcUrl = info.srcUrl;
+  console.log(srcUrl);
+  chrome.tabs.create({ url: srcUrl });
+
+  let body = {
+    "requests": [
+      {
+        "image": {
+          "source": {
+            "imageUri": srcUrl
+          }
+        },
+        "features": [
+          {
+            "type": "LANDMARK_DETECTION",
+            "maxResults": 1
+          }
+        ]
+      }
+    ]
+  }
+
+  fetch(API_URL + API_KEY, {
+    method: 'post',
+    body: JSON.stringify(body)
+  }).then(function(response) {
+    return response.json();
+  }).then(function(response) {
+    console.log(response.responses[0].landmarkAnnotations[0].description);
+  })
+  .catch((error) => {
+    console.log('Error with axios call');
+  });
 }
 
 let take_me = chrome.contextMenus.create({
